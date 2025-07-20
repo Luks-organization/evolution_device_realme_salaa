a995f2f6738ab38ded048561a97f7942fe173a88
Stim <153687700+StimLuks87@users.noreply.github.com>
2025-07-21 03:16:51 +0500

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,6 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Unzip bloobs
+unzip vendor/realme/salaa/radio/md1img.zip -d vendor/realme/salaa/radio && rm vendor/realme/salaa/radio/md1img.zip
+unzip vendor/realme/salaa/proprietary/odm/lib64/libstfaceunlockppl.zip -d vendor/realme/salaa/proprietary/odm/lib64 && rm vendor/realme/salaa/proprietary/odm/lib64/libstfaceunlockppl.zip
+
 # Apply vndk patch
 cd device/realme/salaa/patches && ./apply.sh && cd && cd evo
 
