d38435278932266239af3393b07d657b520c648d
StimLuks87 <san10031987san@gmail.com>
2025-07-27 04:31:34 +0500

    salaa: mega fix sepolicy and fix clear cache

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,7 +9,7 @@ unzip vendor/realme/salaa/radio/md1img.zip -d vendor/realme/salaa/radio && rm ve
 unzip vendor/realme/salaa/proprietary/odm/lib64/libstfaceunlockppl.zip -d vendor/realme/salaa/proprietary/odm/lib64 && rm vendor/realme/salaa/proprietary/odm/lib64/libstfaceunlockppl.zip
 
 # Apply vndk patch
-cd device/realme/salaa/patches && ./apply.sh && cd && cd evo
+#cd device/realme/salaa/patches && ./apply.sh && cd && cd evo
 
 # Disable and stop systemd-oomd service.
 systemctl disable --now systemd-oomd && sudo apt-get purge systemd-oomd -y
